0d129889826739a3d4a7179ca107166c4fdca586
savin <savinbelilya@gmail.com>
2025-02-04 13:56:19 +0100

    vmreact

---
+++ src/App.jsx
@@ -0,0 +1,74 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+    import "./style.css";
+document.querySelector("#app").innerHTML = `
+    <button class="show-modal">Show modal 1</button>
+		<button class="show-modal">Show modal 2</button>
+		<button class="show-modal">Show modal 3</button>
+		<div class="modal hidden">
+		  <button class="close-modal">&times;</button>
+		  <h1>I'm a modal window 😍</h1>
+		  <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+        mollit anim id est laborum.
+      </p>
+		</div>
+		<div class="overlay hidden"></div>
+`;
+
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnCloseModal = document.querySelector(".close-modal");
+const btnsOpenModal = document.querySelectorAll(".show-modal");
+
+// definimos la función closeModal
+const closeModal = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+
+// definimos la función openModal
+const openModal = function () {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+
+// asociamos la función openModal a cada uno de los botones usando forEach
+btnsOpenModal.forEach((btn) => btn.addEventListener("click", openModal));
+
+btnCloseModal.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
+
+// for (let i = 0; i < btnsOpenModal.length; i++) {
+//   btnsOpenModal[i].addEventListener("click", openModal);
+// }
+
+console.log(modal, overlay, btnCloseModal, btnsOpenModal);
+
+// const handleKeyDown = (e) => {
+//   if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+//     closeModal();
+//   }
+// };
+
+// if ESC key is pressed, close the modal
+document.addEventListener("keydown", handleKeyDown);
+
+function handleKeyDown(e) {
+  if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+    closeModal();
+  }
+}
+    </>
+  )
+}
+
+export default App;
